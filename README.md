8f15fd3c8f43f6f1d2ff118b91e2a294bd8a9f6e
moofoo <nathancookdev@gmail.com>
2023-05-25 02:01:50 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # basic-jsreport-core-example
-JsReport Core example
+
+```console
+git clone https://github.com/moofoo/basic-jsreport-core-example.git && cd basic-jsreport-core-example && yarn && node index.js
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+const { readFileSync, writeFileSync, unlinkSync } = require("fs");
+
+// remove existing report files, if they exist
+try {
+  unlinkSync("./report.docx");
+  unlinkSync("./report.pdf");
+} catch (err) {
+  console.log("no report files");
+}
+
+// Create jsreport instance
+const jsreport = require("@jsreport/jsreport-core")();
+
+// Add plugins: docx recipe, handlebars engine and unoconv extension for pdf conversion
+jsreport.use(require("@jsreport/jsreport-docx")());
+jsreport.use(require("@jsreport/jsreport-handlebars")());
+jsreport.use(require("@jsreport/jsreport-unoconv")());
+
+(async () => {
+  // Initialize jsreport instance
+  await jsreport.init();
+
+  // Read template.docx, base64 encoded
+  const content = readFileSync("./template.docx", {encoding: "base64"});
+
+  // Render a .docx report from template
+  const resultDocx = await jsreport.render({
+    template: {
+      content: "",
+      recipe: "docx",
+      engine: "handlebars",
+      docx: {
+        templateAsset: {
+          content,
+          encoding: "base64",
+        },
+      },
+    },
+    data: {
+      helloWorld: "Hello, World!",
+    },
+  });
+
+  // Write report.docx to disk
+  await writeFileSync("./report.docx", resultDocx.content, {encoding: "utf-8"});
+  console.log("report.docx created");
+
+  // Render a .pdf report from template using unoconv extension
+  const resultPdf = await jsreport.render({
+    template: {
+      content: "",
+      recipe: "docx",
+      engine: "handlebars",
+      docx: {
+        templateAsset: {
+          content,
+          encoding: "base64",
+        },
+      },
+      unoconv: {
+        format: "pdf",
+        enabled: true,
+      },
+    },
+    data: {
+      helloWorld: "Hello, World!",
+    },
+  });
+
+  // Write report.pdf to disk
+  await writeFileSync("./report.pdf", resultPdf.content, "binary");
+  console.log("report.pdf created");
+})();
